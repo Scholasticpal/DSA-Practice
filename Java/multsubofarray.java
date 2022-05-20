1113ed683800b2b03535b00946d4bd86d6290c7a
Anmol Garg <80644106+Scholasticpal@users.noreply.github.com>
2022-05-20 17:35:59 +0530

    Update multsubofarray.java

--- Java/multsubofarray.java
+++ Java/multsubofarray.java
@@ -1,15 +1,14 @@
 // Input array, break into subarrays where element is 0, find max multiple of all subarrays.
 
 import java.util.*;
-import java.util.Arrays;
 
 class multsubofarray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of elements you want to store");
         int n = sc.nextInt();
+
         int[] arr = new int[n];
-        int[] a;
         int max = 0;
         int mult = 1;
 
@@ -31,6 +30,7 @@ class multsubofarray {
             max = mult;
         }
         System.out.println(max);
+        sc.close();
 
     }
 }
